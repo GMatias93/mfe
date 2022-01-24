899e7d135ba9c8e21e6d496bd8c5dcabd6013699
gmatias93 <11906352+GMatias93@users.noreply.github.com>
2022-01-24 18:47:03 -0500

    implement auth and create workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
 	StylesProvider,
@@ -14,15 +14,24 @@ const AuthLazy = lazy(() => import('./components/AuthApp'));
 const generateClassName = createGenerateClassName({ productionPrefix: 'co' });
 
 export default () => {
+	const [isSignedIn, setIsSignedIn] = useState(false);
+
 	return (
 		<BrowserRouter>
 			<StylesProvider generateClassName={generateClassName}>
 				<div>
-					<Header />
+					<Header
+						isSignedIn={isSignedIn}
+						onSignOut={() => setIsSignedIn(false)}
+					/>
 					<Suspense fallback={<Progress />}>
 						<Switch>
-							<Route path='/auth' component={AuthLazy} />
-							<Route path='/' component={MarketingLazy} />
+							<Route path='/auth'>
+								<AuthLazy isSignedIn={isSignedIn} onSignIn={setIsSignedIn} />
+							</Route>
+							<Route path='/'>
+								<MarketingLazy isSignedIn={isSignedIn} />
+							</Route>
 						</Switch>
 					</Suspense>
 				</div>
