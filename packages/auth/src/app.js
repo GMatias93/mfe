483032627c5de2c46b9bc1f72c41fcdf41963ec3
gmatias93 <11906352+GMatias93@users.noreply.github.com>
2022-01-24 16:47:58 -0500

    added routing between container and marketing, started auth

--- packages/auth/src/app.js
+++ packages/auth/src/app.js
@@ -5,20 +5,17 @@ import {
 	createGenerateClassName,
 } from '@material-ui/core/styles';
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+// import Landing from './components/Landing';
+// import Pricing from './components/Pricing';
 
-const generateClassName = createGenerateClassName({ productionPrefix: 'ma' });
+const generateClassName = createGenerateClassName({ productionPrefix: 'au' });
 
 export default ({ history }) => {
 	return (
 		<div>
 			<StylesProvider generateClassName={generateClassName}>
 				<Router history={history}>
-					<Switch>
-						<Route exact path='/pricing' component={Pricing} />
-						<Route path='/' component={Landing} />
-					</Switch>
+					<Switch></Switch>
 				</Router>
 			</StylesProvider>
 		</div>
